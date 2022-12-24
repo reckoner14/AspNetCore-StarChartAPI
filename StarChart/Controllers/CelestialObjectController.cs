e65ec135b765f52cfcce2015a74c957cf375c216
reckoner14 <anunaygoyal1403@gmail.com>
2022-12-24 11:22:56 +0000

    Completed StartChart web project

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,129 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using StarChart.Data;
+using StarChart.Models;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Name == name);
+
+            if (!celestialObjects.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(celestialObjects);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject celestialObject)
+        {
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById",new { id=celestialObject.Id }, celestialObject); 
+                
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject celestialObject)
+        {
+            var current = _context.CelestialObjects.Find(id);
+
+            if (current == null)
+            {
+                return NotFound();
+            }
+
+            current.Name = celestialObject.Name;
+            current.OrbitalPeriod = celestialObject.OrbitalPeriod;
+            current.OrbitedObjectId = celestialObject.OrbitedObjectId;
+
+            _context.CelestialObjects.Update(current);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name) 
+        {
+            var current = _context.CelestialObjects.Find(id);
+
+            if(current == null) 
+            {
+                return NotFound();
+            }
+
+            current.Name = name;
+
+            _context.CelestialObjects.Update(current);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Id == id).ToList();
+            if (celestialObjects.Count == 0)
+            {
+                return NotFound();
+            }
+
+            _context.CelestialObjects.RemoveRange(celestialObjects);
+            _context.SaveChanges(); 
+
+            return NoContent();
+        }
+    }
+}
